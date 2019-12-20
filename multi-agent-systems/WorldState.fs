55f82e0b94ea694ff1b7a390191218fdb24067da
joshjennings98 <jenninjl01@gmail.com>
2019-12-20 14:51:16 +0000

    Added WorldState.fs to fsproj and fixed broken merge stuff (I think)

--- multi-agent-systems/WorldState.fs
+++ multi-agent-systems/WorldState.fs
@@ -1,27 +1,9 @@
 ﻿module WorldState
 
 open Types
-open Agent1
+open Agent
 
 let currentWorld = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    VotingType = Borda;
-    Buildings = [];
-    Policies = List.empty<Rule * bool>;
-    System = List.empty<ImmutableRule>;
-    CurrentTurn = 1;
-    NumStag = 10;
-    NumHare = 10;
-    }
-
-let energyProfile (agents : Agent1 list) =
-    agents
-    |> List.map (fun el -> el.ID * el.Energy)
-=======
-=======
->>>>>>> master
-    VotingType = Approval;
     Buildings = [];
     CurrentChair = None;
     TimeToNewChair = 7;
@@ -29,15 +11,16 @@ let energyProfile (agents : Agent1 list) =
     CurrentVotingRule = Approval;
     CurrentFoodRule = Communism;
     CurrentWorkRule = Everyone;
-<<<<<<< HEAD
     CurrentMaxPunishment = NoFoodAndShelter;
-    CurrentSactionStepSize = 0.1;
+    CurrentSanctionStepSize = 0.1;
+    //Policies = List.empty<Rule * bool>;
+    //System = List.empty<ImmutableRule>;
+    CurrentDay = 1;
+    NumStag = 10;
+    NumHare = 10;
     }
-=======
-<<<<<<< HEAD
-    }
->>>>>>> master
-=======
-    }
->>>>>>> master
->>>>>>> game-loop
+
+let energyProfile (agents : Agent list) =
+    agents
+    |> List.map (fun el -> el.ID,el.Energy)
+
